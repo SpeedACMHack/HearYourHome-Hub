94bb4f5359f9b46a2ea149aa4cf57df065d5dad9
jackdare02 <berdichevsky.jacob76@gmail.com>
2016-04-17 06:12:49 -0400

    Plays a russian song, need to change the media to what you want it to play. Has a weird bug may want to put in a console pause for it to play the tone and then kill the signal. May be a few bugs, should work on your phone.

---
+++ HearYourHome/app/src/main/java/org/speedacm/hacking/hearyourhome/ListenToTone.java
@@ -0,0 +1,88 @@
+package org.speedacm.hacking.hearyourhome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.lisnr.hflat.android.Lisnr;
+import com.lisnr.hflat.android.LisnrCallback;
+import com.lisnr.hflat.android.LisnrService;
+import com.lisnr.hflat.sdk.LisnrTone;
+
+public class ListenToTone extends AppCompatActivity {
+    private final static String key = "ef3176d1-1cd0-43d2-899b-7b1b02e65d83";
+    public LisnrTone appearTone;
+    smartListening listen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try{
+            setContentView(R.layout.activity_listen_to_tone);
+        }catch(Exception e){
+            Log.d(e.toString(),e.toString());
+        }
+        LisnrService sdk = Lisnr.getInstance(this);
+        sdk.initWithApiKey(key, this.getApplication());
+        //listen.startListening();
+        listen = new smartListening(sdk,this);
+        sdk.startListening();
+        sdk.setBackgroundEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_listen_to_tone, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** implementation of the callback */
+    private LisnrCallback callback = new LisnrCallback() {
+        @Override
+        public void didHearTone(LisnrTone lisnrTone) {
+            /*appearTone =lisnrTone;
+            listen.didIHearTone(appearTone.getId());*/
+        /* called every time a tone is heard */
+        }
+        @Override
+        public void onToneAppeared(LisnrTone lisnrTone) {
+            appearTone =lisnrTone;
+            listen.didIHearTone(appearTone.getId());
+/* called once while a tone is continously being heard */
+        }
+        @Override
+        public void onToneDisappeared(String s, double v) {
+            listen.killTone(appearTone.getId());
+            appearTone = null;
+/* called once when a continously heard tone disappears*/
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+/*register a callback and start listening*/
+        Lisnr.getInstance(this).registerCallback(callback);
+        Lisnr.getInstance(this).startListening();
+    }
+
+}
--- HearYourHome/app/src/main/java/org/speedacm/hacking/hearyourhome/smartListening.java
+++ HearYourHome/app/src/main/java/org/speedacm/hacking/hearyourhome/smartListening.java
@@ -1,23 +1,10 @@
 package org.speedacm.hacking.hearyourhome;
 
-import android.app.Application;
-import android.content.Context;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 import android.util.Log;
 
 import com.lisnr.hflat.android.LisnrCallback;
-import com.lisnr.hflat.android.services.exceptions.InvalidApiKey;
-import com.lisnr.hflat.sdk.LisnrTone;
-
-import com.lisnr.hflat.android.Lisnr;
 import com.lisnr.hflat.android.LisnrService;
-import com.lisnr.hflat.android.services.smartlistening.SmartListeningCallback;
 
 
 /**
@@ -45,7 +32,15 @@ public class smartListening {
     void killTone(String id){
         //MediaPlayer mediaPlayer = MediaPlayer.create(listen.getApplicationContext(),Integer.parseInt(id));
         if(id.equals("131994")) {
-            MediaPlayer mediaPlayer = MediaPlayer.create(listen.getApplicationContext(), R.raw.media_131994);
+            MediaPlayer mediaPlayer;
+            try {
+                mediaPlayer = MediaPlayer.create(listen.getApplicationContext(), R.raw.media_12345);
+                mediaPlayer.start();
+                Log.d(Boolean.toString(mediaPlayer.isPlaying()),Boolean.toString(mediaPlayer.isPlaying()));
+            }catch(Exception e){
+                Log.d(e.toString(),e.toString());
+            }
+            //mediaPlayer.stop();
             Log.d("Playing Tone","Playing Tone");
         }
     }
